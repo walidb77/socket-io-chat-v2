d6fc07770bc1d019c726a1d3218c267e76d92bb6
Walid Bouguerra <bouguerra.walid77@gmail.com>
2023-04-11 23:20:19 +0200

    Add client part

--- public/client.js
+++ public/client.js
@@ -2,22 +2,74 @@
 const socket = io();
 
 // DOM
-const messages = document.getElementById('messages');
-const form = document.getElementById('form');
-const input = document.getElementById('input');
+const joinContainer = document.getElementById('join-container');
+const joinForm = document.getElementById('join-form');
+const usernameInput = document.getElementById('username');
+const roomSelect = document.getElementById('room-select');
+const chatContainer = document.getElementById('chat-container');
+const chatForm = document.getElementById("chat-form");
+const msgInput = document.getElementById("msg-input");
+const messages = document.getElementById("messages");
+const roomName = document.getElementById("room-name")
+const usersList = document.getElementById("users-list");
+const typingText = document.getElementById("typing-text");
+chatContainer.style.display = "none";
+let username = "";
+
+// Join chat room
+joinForm.addEventListener("submit", e => {
+  e.preventDefault();
+  username = usernameInput.value;
+  socket.emit('join-room', {username, room: roomSelect.value})
+  joinContainer.style.display = "none"
+  chatContainer.style.display = "block";
+});
 
 // Emit message to the server
-form.addEventListener('submit', function(e) {
+chatForm.addEventListener('submit', e => {
   e.preventDefault();
-  if (input.value) {
-    socket.emit('chat-message', input.value);
-    input.value = '';
+  if (msgInput.value) {
+    socket.emit('chat-message', msgInput.value);
+    msgInput.value = '';
+    socket.emit("no-typing");
+  }
+}); 
+
+// User typing
+msgInput.addEventListener('input', () => {
+  if(msgInput.value) {
+    socket.emit("typing");
+  } else {
+    socket.emit("no-typing")
   }
 });
 
-// Message listening form the server
-socket.on('chat-message', msg => {
-    messages.innerHTML += `<li>${msg}</li>`;
-    window.scrollTo(0, document.body.scrollHeight);
+// Reset typing text 
+socket.on("no-typing", () => {
+  typingText.textContent = "";
+  if(msgInput.value) {
+    socket.emit("typing");
+  } 
+});
+
+// Display who is typing
+socket.on("typing", username => {
+  typingText.textContent = `${username} is typing...`;
+});
+
+// Output message to DOM
+socket.on('chat-message', data => {
+    messages.innerHTML += `<div class="${data.username === username ? "bg-primary ms-auto" : "bg-secondary bg-opacity-25 text-dark"} p-3 mb-3 w-50 rounded">
+    <div class="fw-light mb-2">${data.username} - ${data.time}</div>
+    ${data.message}
+  </div>`;
     messages.scrollTop = messages.scrollHeight;
 });
+
+// Output room and users to DOM
+socket.on("room-users", data => {
+  roomName.textContent = data.room;
+  usersList.innerHTML = data.users.map(user => {
+    return `<div>${user.username}</div>`
+  }).join("");
+})
